29539b1392bcab6ae0cacb12f21404c019d80482
Marvin-TC <51245868+Marvin-TC@users.noreply.github.com>
2025-03-20 20:46:50 -0600

    creacion de poblar listadodeProducto, mostraListaProducto
    se empezara a testear.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public List<String> listaProducto;
+    public static List<String> listaProducto =new ArrayList<>();
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        poblarListadoProducto();
+        int opcion = 0;
+        do{
+            mostrarMenu();
+            opcion = sc.nextInt();
+
+            if (opcion==1)
+            {
+
+            } else if (opcion==2) {
+                //mostar lista de operacion
+                mostarListaDeProducto();
+            }else if (opcion==3) {
+
+            }else if (opcion==4) {
+
+            }
+        }while (opcion!=5);{
+            if (sc!=null){sc.close();}
+            System.out.println("saliendo del programa");}
 
 
     }
@@ -20,5 +42,30 @@ public class Main {
         System.out.println("* 5) salir                            *");
         System.out.println("***************************************");
         System.out.print("Ingrese una opcion del menu: ");
+    }
+
+    public static void poblarListadoProducto()
+    {
+        listaProducto.add("1020,Dorito rojo,10,4.00");
+        listaProducto.add("1021,Jugo del frutal,5,5.00");
+        listaProducto.add("1022,Sopa Instantanea,8,5.00");
+        listaProducto.add("1023,Pepsi 3L,1,18.00");
+    }
+
+    public static void mostarListaDeProducto(){
+        if (listaProducto.isEmpty())
+        {System.out.println("lista de productos vacios");}
+        else {
+            for (String productoTemp: listaProducto){
+                String[] atributos = productoTemp.split(",");
+                System.out.println("Codigo:    "+atributos[0]);
+                System.out.println("Nombre:    "+atributos[1]);
+                System.out.println("Stock:     "+atributos[2]);
+                System.out.println("Precio:     Q "+atributos[3]);
+                System.out.println("");
+            }
+            System.out.println("-------- fin de operacion -----------");
+        }
+
     }
 }
