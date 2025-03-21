6de077821cbed51a851b1c5b9d7fb565d4b9f520
Marvin-TC <51245868+Marvin-TC@users.noreply.github.com>
2025-03-20 20:36:09 -0600

    creacion de menu de opciones para el cliente

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public List<String> listaProducto;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        //comentario
+
+
+
+    }
+    public static void mostrarMenu()
+    {
+        System.out.println("***************** MENU ****************");
+        System.out.println("* 1) Agregar Producto                 *");
+        System.out.println("* 2) Mostrar Producto                 *");
+        System.out.println("* 3) Actualizar Cantidad de producto  *");
+        System.out.println("* 4) eliminar Producto                *");
+        System.out.println("* 5) salir                            *");
+        System.out.println("***************************************");
+        System.out.print("Ingrese una opcion del menu: ");
     }
 }
